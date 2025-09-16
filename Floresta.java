51523b1074bac222b86df8554f0fe1e3112d2b82
Gustavo Kiyoshi Ikeda <gustavokikeda@gmail.com>
2025-09-15 22:33:22 -0300

    Update Floresta.java

--- Floresta.java
+++ Floresta.java
@@ -1,7 +1,6 @@
 public class Floresta {
     int quantidade_arvores;
 
-
     public Floresta(){
         this.quantidade_arvores = 0;
     }
@@ -15,12 +14,39 @@ public class Floresta {
             nova_arvore.inserir_no(filho);
             this.quantidade_arvores++;
         }
-        else if(raizes.getQuantidade_raizes() != 0 && raizes.get_No(pai, raizes) == null){
+        else if(raizes.getQuantidade_raizes() != 0 && raizes.get_No(pai) == null){
             Arvore nova_arvore = new Arvore();
             raizes.inserir_raiz(pai);
             nova_arvore.inserir_no(pai);
             nova_arvore.inserir_no(filho);
             this.quantidade_arvores++;
+        }
+        else
+        {
+            No buscar_filho = raizes.get_No(filho);
+            if(buscar_filho == null)
+            {
+                if(pai.esquerda == null)
+                {
+                    pai.esquerda = filho;
+                }
+                else if(pai.direita == null)
+                {
+                    pai.direita = filho;
+                }
+            }
+            else
+            {
+                if(pai.esquerda == null)
+                {
+                    pai.esquerda = filho;
+                }
+                else if(pai.direita == null)
+                {
+                    pai.direita = filho;
+                }
+                raizes.remover_raiz(filho);
+            }
         }
 
     }
